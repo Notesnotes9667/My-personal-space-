52e4566265dfaf3b558676ad708c0f773cd48163
Notesnotes9667 <65580637+Notesnotes9667@users.noreply.github.com>
2026-01-02 11:03:56 +0530

    Update API credentials and owner details

--- modules/vars.py
+++ modules/vars.py
@@ -3,11 +3,11 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "123"))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8366707481:AAFUiyIW-hjnOWfGbdBi4sc7nNQK8YfxXFw")
 
-OWNER = int(environ.get("OWNER", ""))
+OWNER = int(environ.get("OWNER", "7678666715"))
 CREDIT = environ.get("CREDIT", "Crystal")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
@@ -20,6 +20,7 @@ if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.......,.....,
+
 
 
 
